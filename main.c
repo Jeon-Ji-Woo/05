eeb51b2681ec633398ea7e0fed598db7949c74c4
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2023-10-08 23:58:01 +0900

    실습6

--- main.c
+++ main.c
@@ -4,20 +4,26 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum = 0;
+	int answer = 32;
 	int num;
-	int i;
+	int attempts = 0;
 	
-	printf("input a number :");
-	scanf("%d", &num);
-	
-	for (i=0; i<=num; i++)
+	do
 	{
-		sum = sum + i;
+		printf("Guess a number :");
+		scanf("%d", &num);
+		attempts++;
+		
+		if(num > answer)
+			printf("low!");
+		else if(num < answer)
+			printf("high!");
+		else
+			printf("congratulation! trials :(&attempts)");
+	
 	}
-	
-	printf("The result is %i\n", sum);
-	
+	while (num != answer);
+		
 	system("PAUSE");
 	return 0;
 }
