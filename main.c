18d657764f8947503bf515c09daae3126d7799cc
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2025-10-05 19:29:29 +0900

    실습6

--- main.c
+++ main.c
@@ -5,33 +5,23 @@
 
 int main(int argc, char *argv[]) {
 	
-	int a, b;
-	int op;
-	int result;
+	int answer = 59;
+	int num;
+	int trial=0;
 	
-	printf("Enter the calculation :");
-	scanf("%i %c %i", &a,&op,&b);
-	
-	//if (op =='+')
-	//	result = a+b;
-	//else if(op =='-')
-	//	result = a-b;
-	
-	switch(op)
+	do
 	{
-		case '+':
-			result = a+b;
-			break;
-			
-		case '-':
-			result = a-b;
-			break;
+		printf("Input a number:");
+		scanf("%d", &num);
 		
+		if (answer<num)
+			printf("high!\n");
+		else if (answer>num)
+			printf("low!\n");
+		trial++;
 	}
+	while(answer != num);
 	
-
-	printf("%i %c %i\n", a, op, b);
-	
-	printf("=%i", result);
+	printf("Congratulation! trial:%i\n", trial);
 	return 0;
 }
